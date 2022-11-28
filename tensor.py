fixed backprop for Dot product, my autograd engine works! yehaw (at least i think it works)

tensor.py
@@ -18,7 +18,8 @@ class MSE(Function):
 
     @classmethod
     def backward(cls, op1: 'Tensor', op2: 'Tensor', previous_grad: np.ndarray):
-        op1.grad = np.divide(np.multiply(np.sum(np.subtract(op2.data, op1.data), 0), -2), op1.data.shape[0])
+        op1.grad = np.divide(np.multiply(
+            np.sum(np.subtract(op2.data, op1.data), 0), -2), op1.data.shape[0])
         # print(f"MSE OP1: {op1.grad}")
         op1.backward()
 
@@ -31,7 +32,7 @@ class Tanh(Function):
 
     @classmethod
     def backward(cls, op: 'Tensor', previous_grad: np.ndarray):
-        op.grad = 1 - np.tanh(op.data) * np.tanh(op.data)
+        op.grad = (1 - np.tanh(op.data) * np.tanh(op.data)) * previous_grad
         # print(f"TANH OP: {op.grad}")
         op.backward()
 
@@ -44,13 +45,19 @@ class Dot(Function):
 
     @classmethod
     def backward(cls, op1: 'Tensor', op2: 'Tensor', previous_grad: np.ndarray):
-        # op1 is the activations(L-1), op2 is the weights 
-        op2.grad = op1.data.reshape(1, op1.data.shape[0]).repeat(op2.data.shape[0], 0) * previous_grad
-        op1.grad = (op2.data * previous_grad).sum(axis=0)
-        op2.grad = np.dot(op1.data, previous_grad.T).T
+        # op1 is the activations(L-1), op2 is the weights
+        # op2.grad = op1.data.reshape((1, op1.data.shape[0])).repeat(
+        #     op2.data.shape[0], 0) * previous_grad
+        op2.grad = op1.data.T.repeat(op2.data.shape[0], 0) * previous_grad
+        # print(f"op1: {op1.data}, pgrad: {previous_grad}")
+        # print(f"OP2:GRAD: {op2.grad}")
+        # print(f"{op2.data}")
+        op1.grad = (op2.data).sum(axis=0)  # * previous_grad
+        op1.grad = op1.grad.reshape(op2.data.shape[1], 1) * sum(previous_grad)
+        # print(f"in f: op1 grad: {op1.grad}")
+        # op2.grad = np.dot(op1.data, previous_grad.T).T
         op1.backward()
         op2.backward()
-
 
     @classmethod
     def apply(cls, a: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -72,6 +79,17 @@ class Add(Function):
     @classmethod
     def apply(cls, a: np.ndarray, b: np.ndarray) -> np.ndarray:
         return np.add(a, b)
+
+
+class Sum(Function):
+    @classmethod
+    def backward(cls, op1: 'Tensor', previous_grad: np.ndarray):
+        op1.grad = np.repeat(previous_grad, op1.data.shape[0], 0)
+        op1.backward()
+
+    @classmethod
+    def apply(cls, op: np.ndarray):
+        return np.sum(op)
 
 
 class Tensor:
@@ -101,6 +119,9 @@ class Tensor:
     def mse(self, b: 'Tensor'):
         return Tensor(MSE.apply(self.data, b.data), MSE, [self, b])
 
+    def sum(self):
+        return Tensor(Sum.apply(self.data), Sum, [self])
+
 
 class SimpleOptimizer:
 
@@ -110,7 +131,7 @@ class SimpleOptimizer:
     def optimize(self, tensor: Tensor, l1: Tensor):
         tensors = [tensor]
         while tensors:
-            tensors[0].grad = np.subtract(tensors[0].data, np.multiply(
+            tensors[0].data = np.subtract(tensors[0].data, np.multiply(
                 tensors[0].grad, self.step))
             if tensors[0].function_args:
                 tensors += tensors[0].function_args
@@ -124,23 +145,17 @@ class SimpleOptimizer:
 # print(f"L1: {l1}")
 # bias_l1 = Tensor(np.zeros((2, 1)))
 # # print(f"BIAS L1: {bias_l1}")
-# # l2 = Tensor(np.random.rand(8, 6))
+# l2 = Tensor(np.random.rand(1, 2))
 # # bias_l2 = Tensor(np.zeros((8, 1)))
-# y = Tensor(np.array([[1],[0]]))
+# y = Tensor(np.array([[1], [0]]))
 
-# out = inp.dot(l1).add(bias_l1).tanh()  # .dot(l2).add(bias_l2).tanh()
-# print(out)
-# err = out.mse(y)
-# print(err)
-# err.backward()
-
-# print("GRADS:")
-# print(f"MSE: {err.grad}")
-# print(f"Tanh {out.grad}")
-# print(f"ADD RESULT: {out.function_args[0].grad}")
-# print(f"ADD OP1 (DOT RES): {out.function_args[0].function_args[0].grad}")
-# print(f"ADD OP2 (BIAS): {out.function_args[0].function_args[1].grad}")
-# print(
-#     f"DOT WEIGHTS: {out.function_args[0].function_args[0].function_args[1].grad}")
-# print(
-#     f"DOT ACTIVATIONS: {out.function_args[0].function_args[0].function_args[0].grad}")
+# # .add(bias_l1).tanh()  # .dot(l2).add(bias_l2).tanh()
+# d1 = inp.dot(l1)  # .dot(l2).sum()
+# d2 = d1.dot(l2)
+# out = d2.sum()
+# out.backward()
+# print(f"L1 grad: {l1.grad}")
+# print(f"L2 grad: {l2.grad}")
+# print(f"d1.grad: {d1.grad}")
+# print(f"d2.grad: {d2.grad}")
+# exit(0)

xor_test.py
@@ -40,14 +40,14 @@ combinations = [[0, 0, 0],
                 [1, 1, 0]]
 # same dataset for testing and training cuz it's not important to have separate sets
 # when I just want to see if my network can learn (if autograd works) - spoiler alert: it doesn't work
-dataset = np.repeat(combinations, 1000, 0)
+dataset = np.repeat(combinations, 100, 0)
 np.random.shuffle(dataset)
 # dataset = dataset[0:1]
 nnet = NNET()
 
 nnet.train(dataset)
-# x = Tensor(dataset[78][:2].reshape(2, 1))
-# y = nnet.label_to_tensor(dataset[78][0])
-# out = nnet.forward(x)
-# mse = out.mse(y)
-# print(x, y, out)
+x = Tensor(dataset[78][:2].reshape(2, 1))
+y = nnet.label_to_tensor(dataset[78][0])
+out = nnet.forward(x)
+mse = out.mse(y)
+print(x, y, out)